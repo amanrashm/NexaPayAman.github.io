c63d0911ab19ef57b3cc2fc216291fd9aa5561aa
aman0990 <raj.aman.726@gmail.com>
2023-02-04 14:46:50 +0530

    MyNexa
    NexaUpdates

---
+++ dt.12/myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './NavBar';
+function App() {
+  
+  return (
+    <> 
+    <Navbar title ="Navbar" aboutText="AboutNexaPay"/>
+    </>
+     
+  );
+}
+
+export default App;
